b8e0dd50651798162ecf19e97aaedb6806640fa7
Michael <michaelspantrix@gmail.com>
2023-06-18 07:39:32 -0700

    prints all possible combination of single digit numbers separated by commas in ascending order

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible combinations of single-digit numbers
+ * Numbers are separated by ", " and printed in ascending order
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
