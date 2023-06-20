25337e68651b55b923f0d33171a5d608bf428f04
Michael <michaelspantrix@gmail.com>
2023-06-20 07:24:43 -0700

    returns the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+/**
+ * _abs - Computes the absolute value of an integer
+ *
+ * @n: The integer
+ *
+ * Return: The absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
+
