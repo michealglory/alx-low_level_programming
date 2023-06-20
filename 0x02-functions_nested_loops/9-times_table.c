0ebeb3eeda44bb3f28e8e562f8a2f160afd9074f
Michael <michaelspantrix@gmail.com>
2023-06-20 11:49:48 -0700

    prints the multiplication table from 0 to 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the multiplication table from 0 to 9
+ */
+void times_table(void)
+{
+	int i, j, timesValue;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			int value = i * j;
+
+			if (value < 10)
+			{
+				if (j != 0)
+				{
+					_putchar(' ');
+				}
+				_putchar(value + '0');
+			}
+			else
+			{
+				_putchar((value / 10) + '0');
+				_putchar((value % 10) + '0');
+			}
+
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}
+
