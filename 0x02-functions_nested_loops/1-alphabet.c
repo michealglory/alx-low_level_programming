13e6683d2c560d3ce92c8f7a11839114799081da
Michael <michaelspantrix@gmail.com>
2023-06-20 05:58:39 -0700

    prints all alphabets in lowercase from a to z using only _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints a to z in lowercase using only _putchar
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(i);
+
+}
+
