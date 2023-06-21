2e91cb586c6f3bc3905118a61b504ed8f907807e
Michael <michaelspantrix@gmail.com>
2023-06-20 19:09:45 -0700

    sums up even fibonacci numbers less than 4000000

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -3,8 +3,9 @@
 /**
  * main - Entry point of the program
  *
- * Description: Calculates the sum of even-valued terms in the Fibonacci sequence
- *              up to a limit of 4,000,000 and prints the result.
+ * Description: Calculates the sum of even-valued terms
+ * in the Fibonacci sequence up to a limit of 4,000,000
+ * and prints the result.
  *
  * Return: 0 (Success)
  */
