3cf0a3a378bca0f641435a204f21dea053e1ac69
Michael <michaelspantrix@gmail.com>
2023-06-23 02:48:44 -0700

    prints an integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void print_number(int n)
+{
+	if (n < 0) {
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10 != 0) {
+		print_number(n / 10);
+	}
+
+	_putchar('0' + n % 10);
+}
+
