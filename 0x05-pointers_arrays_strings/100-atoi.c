f275474235618067eda02b322a1eac7ed86ad79d
Michael <michaelspantrix@gmail.com>
2023-06-26 14:49:00 -0700

    converts string to integers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,3 @@
-#include "main.h"
-
 /**
  * _atoi - Converts a string to an integer
  * @s: The string to convert
@@ -8,29 +6,31 @@
  */
 int _atoi(char *s)
 {
-	int sign = 1;  /* Sign of the number, initially positive */
+	int sign = 1; /* Sign of the number, initially positive */
 	int result = 0;
 	int i = 0;
 
-	/* Skip leading whitespace */
 	while (s[i] == ' ')
 		i++;
 
-	/* Check for optional sign character */
 	while (s[i] == '-' || s[i] == '+')
 	{
 		if (s[i] == '-')
 			sign *= -1;
+		else if (s[i] == '+')
+			sign *= 1;
 		i++;
 	}
 
-	/* Process digits */
 	while (s[i] != '\0')
 	{
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			/* Accumulate value by multiplying result by 10 and adding digit */
 			result = result * 10 + (s[i] - '0');
+		}
+		else if (s[i] == '-')
+		{
+			sign *= -1;
 		}
 		else if (result != 0)
 		{
@@ -40,7 +40,6 @@ int _atoi(char *s)
 		i++;
 	}
 
-	/* Apply sign to the result */
 	result *= sign;
 
 	return (result);
