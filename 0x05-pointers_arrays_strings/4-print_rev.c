21e7b46b5366ac113e82527c9455a15e135d37e1
Michael <michaelspantrix@gmail.com>
2023-06-26 07:58:32 -0700

    prints a string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,14 +6,17 @@
  */
 void print_rev(char *s)
 {
-	int length = 0;
+	int length;
+	int i;
+
+	length = 0;
 
 	while (s[length] != '\0')
 	{
 		length++;
 	}
 
-	for (int i = length - 1; i >= 0; i--)
+	for (i = length - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
