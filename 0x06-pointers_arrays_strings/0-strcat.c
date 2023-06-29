84e2197356a6060251f936524cd62563dd18ed21
Michael <michaelspantrix@gmail.com>
2023-06-29 06:33:13 -0700

    concatenates character

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: The destination string.
+ * @src: The source string.
+ *
+ * Return: Pointer to the resulting string @dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int dest_len = 0;
+	int i = 0;
+
+	/* Find the length of the destination string */
+	while (dest[dest_len] != '\0')
+		dest_len++;
+
+	/* Append the source string to the destination string */
+	while (src[i] != '\0')
+	{
+		dest[dest_len] = src[i];
+		dest_len++;
+		i++;
+	}
+
+	/* Add the terminating null byte */
+	dest[dest_len] = '\0';
+
+	return dest;
+}
+
