61531b1443085034691f8ffa2b35ba7035348668
Michael <michaelspantrix@gmail.com>
2023-06-29 11:43:49 -0700

    prints integers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,37 +1,33 @@
-#include "main.h"
 #include <limits.h>
+#include "main.h"
 
 /**
- * print_number - Prints an integer.
- * @n: The integer to be printed.
+ * print_number - Prints an integer
+ * @n: The integer to be printed
+ *
+ * Description: This function prints the given integer. It handles negative
+ * numbers and the special case of INT_MIN correctly.
  */
 void print_number(int n)
 {
-	if (n == 0)
-		_putchar('0');
-	else if (n == INT_MIN)
+	if (n == INT_MIN)
 	{
 		_putchar('-');
-		print_positive_number((unsigned int)INT_MAX + 1);
+		print_number(-(n) / 10);
+		_putchar('0' - (n) % 10);
+		return;
 	}
-	else if (n < 0)
-	{
-		_putchar('-');
-		print_positive_number(-n);
-	}
-	else
-		print_positive_number(n);
-}
 
-/**
- * print_positive_number - Prints a positive integer.
- * @n: The positive integer to be printed.
- */
-void print_positive_number(unsigned int n)
-{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
 	if (n / 10 != 0)
-		print_positive_number(n / 10);
+	{
+		print_number(n / 10);
+	}
 
-	_putchar('0' + (n % 10));
+	_putchar('0' + n % 10);
 }
-
