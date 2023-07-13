022a6260030a8e490cb64db5826eccdc591ccbe3
Michael <michaelspantrix@gmail.com>
2023-07-13 13:31:09 -0700

    allocat4s memory using malloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,17 +2,6 @@
 #define MAIN_H
 
 int _putchar(char c);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
-int factorial(int n);
-int _pow_recursion(int x, int y);
-int _sqrt_recursion(int n);
-int sqrt_helper(int n, int guess);
-int is_prime_number(int n);
-int is_prime_helper(int n, int divisor);
-int is_palindrome(char *s);
-int _strlen(char *s);
-int check_palindrome(char *s, int start, int end);
-int wildcmp(char *s1, char *s2);
+void *malloc_checked(unsigned int b);
 
 #endif /* MAIN_H */
