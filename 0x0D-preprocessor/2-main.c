4c23dc79e4a1da59bf272d4da4781205b8acf4fd
Michael <michaelspantrix@gmail.com>
2023-07-17 15:10:21 -0700

    prints the name of a file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: (0) Successful execution
+ */
+int main(void)
+{
+	printf("Compiled from file: %s\n", __FILE__);
+	return (0);
+}
+
