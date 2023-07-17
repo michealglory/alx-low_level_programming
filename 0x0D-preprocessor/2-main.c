d48489c2145006df2ecef1500342ab2c5e46cea3
Michael <michaelspantrix@gmail.com>
2023-07-17 15:26:19 -0700

    prints filename

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,39 +1,12 @@
 #include <stdio.h>
-#include <string.h>
 
 /**
- * get_filename - Extracts the file name from the given path.
- * @path: The full path of the file.
+ * main - prints the name of the file from which it was compiled
  *
- * Return: The extracted file name.
- */
-const char *get_filename(const char *path)
-{
-	const char *filename = path;
-
-	while (*path != '\0')
-	{
-		if (*path == '/' || *path == '\\')
-			filename = path + 1;
-
-		path++;
-	}
-
-	return (filename);
-}
-
-/**
- * main - Entry point of the program.
- *
- * Return: Always 0 (Success).
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	const char *filename = get_filename(__FILE__);
-
-	printf("Compiled from file: %s\n", filename);
-
-
+	printf("%s\n", __FILE__);
 	return (0);
 }
-
