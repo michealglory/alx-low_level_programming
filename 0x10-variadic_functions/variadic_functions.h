e8fd4a5e8ba132d4fb4e6c25738e5d405c9250b6
Michael <michaelspantrix@gmail.com>
2023-07-20 15:10:38 -0700

    prints variable of any type

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,5 +6,9 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_char(va_list args, const char *separator);
+void print_integer(va_list args, const char *separator);
+void print_float(va_list args, const char *separator);
+void print_string(va_list args, const char *separator);
 
 #endif /* MAIN_H */
