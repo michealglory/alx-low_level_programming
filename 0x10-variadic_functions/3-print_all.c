89e8b94913c8c476d9bf327edcc58c1d946b33f9
Michael <michaelspantrix@gmail.com>
2023-07-20 15:34:59 -0700

    prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,52 +1,57 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
 #include "variadic_functions.h"
 
 /**
- *print_all - prints arguements of any type.
- *@format: list of all arguments.
+ * print_all - Prints data based on the given format string.
  *
- *Return: void.
+ * @format: A string containing format specifiers.
+ *          c: char
+ *          i: integer
+ *          f: float
+ *          s: char* (if the string is NULL, print (nil) instead)
+ *          Any other character should be ignored.
+ *
+ * Return: Always void.
  */
 void print_all(const char * const format, ...)
 {
-	unsigned int i;
 	va_list args;
-	char *sValue, *separator;
+	int i = 0;
+	char *str;
 
 	va_start(args, format);
 
-	separator = "";
-
-	i = 0;
 	while (format && format[i])
 	{
+		if (i > 0)
+			printf(", ");
+
 		switch (format[i])
 		{
 			case 'c':
-				printf("%s%c", separator,  va_arg(args, int));
+				printf("%c", va_arg(args, int));
 				break;
 			case 'i':
-				printf("%s%d", separator, va_arg(args, int));
+				printf("%d", va_arg(args, int));
 				break;
 			case 'f':
-				printf("%s%f", separator, va_arg(args, double));
+				printf("%f", va_arg(args, double));
 				break;
 			case 's':
-				sValue = va_arg(args, char *);
-				if (sValue == NULL)
-					sValue = "(nil)";
-				printf("%s%s", separator, sValue);
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
 				break;
 			default:
-				i++;
-				continue;
+				break;
 		}
-		separator = ", ";
+
 		i++;
 	}
 
-	printf("\n");
 	va_end(args);
+	printf("\n");
 }
+
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,9 +6,5 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_char(va_list args, const char *separator);
-void print_integer(va_list args, const char *separator);
-void print_float(va_list args, const char *separator);
-void print_string(va_list args, const char *separator);
 
 #endif /* MAIN_H */
