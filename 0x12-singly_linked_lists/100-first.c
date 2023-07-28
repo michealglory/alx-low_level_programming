3aa2b67ebfa66089fe16708eb3624c106f1781a7
Michael Medahunsi <michaelspantrix@gmail.com>
2023-07-27 18:19:37 -0700

    executes function before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+/**
+ * print_before_main - Function to print the desired message before main.
+ */
+void print_before_main(void)
+{
+
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
