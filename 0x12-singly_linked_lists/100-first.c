6834086b4f32533f9875acb39c6e7e0ce3d4d458
Michael Medahunsi <michaelspantrix@gmail.com>
2023-07-27 18:26:37 -0700

    executes function before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,25 @@
-/**
- * print_before_main - Function to print the desired message before main.
- */
-void print_before_main(void)
-{
+#include <stdio.h>
 
+/**
+ * first - Function executed automatically before main.
+ *
+ * This function is executed automatically before the `main` function
+ * when the program starts. It is designated with the
+ * `__attribute__((constructor))` attribute, which tells the compiler
+ * to execute it during the program's initialization phase.
+ *
+ * The function's purpose is to print the following message:
+ * "You're beat! and yet, you must allow,
+ * I bore my house upon my back!"
+ *
+ * This message is printed to the standard output using the `printf`
+ * function.
+ * The newline character ('\n') is used to introduce a line break in
+ * the output.
+ */
+
+void __attribute__ ((constructor)) first()
+{
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
 }
